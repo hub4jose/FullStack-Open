8a2bc444fc80bb0c488470b08b20fbe4e6c63d9b
Joseph <jnakarmi123@gmail.com>
2021-01-27 12:05:38 -0500

    added Procfile

--- Part3/Phonebook Backend/index.js
+++ Part3/Phonebook Backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 app.use(morgan('tiny'))
@@ -111,7 +113,7 @@ let persons = [ {
     })
     
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
